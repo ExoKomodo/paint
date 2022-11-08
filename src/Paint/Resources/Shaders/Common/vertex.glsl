23035d59fc19a0ce3b537d40c1ebcb17f0579c11
James Orson <jamesaorson@gmail.com>
2022-11-08 14:35:49 -0800

    Remove comment

--- src/Paint/Resources/Shaders/Common/vertex.glsl
+++ src/Paint/Resources/Shaders/Common/vertex.glsl
@@ -1,5 +1,5 @@
 #version 330 core
-layout (location = 0) in vec3 position;   // the position variable has attribute position 0
+layout (location = 0) in vec3 position;
 
 vec4 mapPosition3D(vec3 positionToMap, float zoom);
 
