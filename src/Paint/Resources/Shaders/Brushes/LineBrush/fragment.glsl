d443c3e979fbe56d4632b9361d77e4f13d93d8b8
James Orson <jamesaorson@gmail.com>
2022-11-19 20:51:45 -0800

    Checkpoint

--- src/Paint/Resources/Shaders/Brushes/LineBrush/fragment.glsl
+++ src/Paint/Resources/Shaders/Brushes/LineBrush/fragment.glsl
@@ -7,13 +7,18 @@ uniform vec3 end;
 
 void main() {
 	// https://byjus.com/maths/two-point-form/
+	// Draw two points
+	// Draw line between them
+	// Attach mouse to one point
+	// Draw points for cross and dot products
+
 	if (false) {
 		discard;
-		FragColor = vec4(
-			0.0f,
-			1.0f,
-			0.3f,
-			1.0f
-		);
 	}
+	FragColor = vec4(
+		0.0f,
+		1.0f,
+		0.3f,
+		1.0f
+	);
 }
