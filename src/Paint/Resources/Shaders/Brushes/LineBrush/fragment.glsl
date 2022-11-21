2d94eaef4945c062c505890e0b724d42bc72da9b
James Orson <jamesaorson@gmail.com>
2022-11-21 08:10:46 -0800

    Add line drawing but now understand it

--- src/Paint/Resources/Shaders/Brushes/LineBrush/fragment.glsl
+++ src/Paint/Resources/Shaders/Brushes/LineBrush/fragment.glsl
@@ -29,17 +29,18 @@ void main()
 		);
 		return;
 	}
-	// Draw point for cross product
-	vec3 _cross = cross(START, vec3(mouse.xy, 1));
-	if (distance(gl_FragCoord.xy, _cross.xy) <= RADIUS) {
-		FragColor = vec4(
-			1.0f,
-			0.0f,
-			0.3f,
-			1.0f
-		);
+	// Draw line between them
+	// TODO: Understand this
+	vec2 ab = mouse.xy - START.xy;
+	vec2 ac = gl_FragCoord.xy - START.xy;
+
+	float _dot = dot(ab, ac) / length(ab); // = length(p13) * cos(angle)
+	vec2 p4 = START.xy + normalize(ab) * _dot;
+	if (length(p4 - gl_FragCoord.xy) < 10/* * sin01(iTime * 4.0 + length(p4 - p1)* 0.02)*/
+				&& length(p4 - START.xy) <= length(ab)
+				&& length(p4 - mouse.xy) <= length(ab)) {
+		FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 		return;
 	}
-	// Draw line between them?
 	discard;
 } 
